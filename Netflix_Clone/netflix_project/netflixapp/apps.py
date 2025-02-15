9934dace662bcbec67920744a9228b424c482b44
Muhammad Aqeel <104295133+Aqeel785@users.noreply.github.com>
2025-02-15 23:08:32 +0500

    create netflix app

--- Netflix_Clone/netflix_project/netflixapp/apps.py
+++ Netflix_Clone/netflix_project/netflixapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class NetflixAppConfig(AppConfig):
+class NetflixappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'netflix_app'
+    name = 'netflixapp'
