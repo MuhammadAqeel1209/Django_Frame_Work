5fb011867960f2b98dd8c828c831f64e81c6e3a1
Muhammad Aqeel <104295133+Aqeel785@users.noreply.github.com>
2025-02-18 23:06:20 +0500

    some settings and models create

--- Netflix_Clone/netflix_project/netflix_project/settings.py
+++ Netflix_Clone/netflix_project/netflix_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(d6u*97-jgu0x9c#y4(cv=ngdw#_5ut#e_si1z@9*3+uk9ilwx'
+SECRET_KEY = 'django-insecure-c^x-=-w%$3e$rob7=amo=pf0la@f_8%@v(u5%ww+j_0wkhg$di'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'netflixapp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'netflix_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +117,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+AUTH_USER_MODEL = "netflixapp.CustomUser"
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# LOGIN_URL = 'login'
+# LOGIN_REDIRECT_URL = 'netflixapp:Home'
+# ACCOUNT_AUTHENTICATION_METHOD='email'
+# ACCOUNT_EMAIL_REQUIRED=True
+# ACCOUNT_EMAIL_VERIFICATION='none'
+# ACCOUNT_USERNAME_REQUIRED=False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
