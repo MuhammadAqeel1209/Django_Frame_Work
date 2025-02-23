36321101dea5250a123d42e48e4e3141264d33ef
Muhammad Aqeel <104295133+Aqeel785@users.noreply.github.com>
2025-02-23 20:53:14 +0500

    Project Delete

--- Netflix_Clone/netflix_project/netflixapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser,Movie,Video,Profile
-
-# Register your models here.
-admin.site.register(Movie)
-admin.site.register(CustomUser)
-admin.site.register(Video)
-admin.site.register(Profile)
--- Netflix_Clone/netflix_project/netflixapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views import View
-
-# Create your views here.
-class Home(View):
-    def get(self, request, *args, **kwargs):
-        # if request.user.is_authenticated:
-        #     return redirect('netflixapp:profile-list')
-        return render(request, 'index.html')
